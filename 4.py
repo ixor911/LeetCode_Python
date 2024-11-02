1824e31e79c2b11804b2352111374b100c4a2012
ixor911 <igorbutenko9111@gmail.com>
2024-11-02 16:22:53 +0100

    4. Median of Two Sorted Arrays Own concat + sort

--- 4.py
+++ 4.py
@@ -13,22 +13,37 @@ Example 3:
 Input: nums1 = [2,2,4,4], nums2 = [2,2,2,4,4]
 Output: 2.00000
 """
+from statistics import median
+
 
 # =========== SOLUTION =========== #
 
 class Solution:
+    def concat(self, nums1: list[int], nums2: list [int]) -> list[int]:
+        new_nums = []
+
+        while nums1 or nums2:
+
+            if not nums1 or not nums2:
+                last = nums1 if nums1 else nums2
+                last.reverse()
+                new_nums.extend(last)
+                return new_nums
+
+            new_num = nums1.pop() if nums1[-1] >= nums2[-1] else nums2.pop()
+            new_nums.append(new_num)
+
     def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
-        nums1 = nums1 + nums2
-        nums1.sort()
+        nums = self.concat(nums1, nums2)
 
-        index = len(nums1) / 2
+        index = len(nums) / 2
         if index != int(index):
-            return nums1[int(index)]
+            return nums[int(index)]
 
-        return (nums1[int(index) - 1] + nums1[int(index)]) / 2
+        return (nums[int(index) - 1] + nums[int(index)]) / 2
 
 
 # =========== TEST =========== #
 
 solution = Solution()
-print(solution.findMedianSortedArrays([1,2], [2]))
+print(solution.findMedianSortedArrays([1,2], [3,4]))
