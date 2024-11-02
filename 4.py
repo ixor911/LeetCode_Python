857c9f8b132075f33e7bd34b21d00097b5b94bd8
ixor911 <igorbutenko9111@gmail.com>
2024-11-02 16:04:34 +0100

    4. Median of Two Sorted Arrays 0ms

--- 4.py
+++ 4.py
@@ -17,32 +17,18 @@ Output: 2.00000
 # =========== SOLUTION =========== #
 
 class Solution:
-    def list_median(self, nums) -> float | None:
-        if len(nums) == 0:
-            return None
-
-        index = len(nums) / 2
-
-        if index == int(index):
-            return (nums[int(index) - 1] + nums[int(index)]) / 2
-        else:
-            return nums[int(index)]
-
-
     def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
-        med1 = self.list_median(nums1)
-        med2 = self.list_median(nums2)
+        nums1 = nums1 + nums2
+        nums1.sort()
 
-        if not med1:
-            return med2
-        elif not med2:
-            return med1
+        index = len(nums1) / 2
+        if index != int(index):
+            return nums1[int(index)]
 
-        return (med1 + med2) / 2
-
+        return (nums1[int(index) - 1] + nums1[int(index)]) / 2
 
 
 # =========== TEST =========== #
 
 solution = Solution()
-print(solution.findMedianSortedArrays([2,2,4,4], [2,2,2,4,4]))
+print(solution.findMedianSortedArrays([1,2], [2]))
